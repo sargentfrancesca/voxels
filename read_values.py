7c2bbaab573b4949fd841f1a9c0367b4d65366fb
Francesca Sargent <ladyquas@Francescas-MBP.lan>
2016-04-26 13:48:17 +0100

    Recent revelations

--- read_values.py
+++ read_values.py
@@ -1,6 +1,31 @@
 from PIL import Image
 import numpy, math
 import scipy.misc as smp
+import sys, os
+
+# Size, always square
+size = int(sys.argv[1])
+# X value of source tile
+x_val = int(sys.argv[2])
+# Y value of source tile
+y_val = int(sys.argv[3])
+# Max Z value of source tile
+z_val = int(sys.argv[4])
+# X value to start crop
+crop_x = int(sys.argv[5])
+# Y value to start crop
+crop_y = int(sys.argv[6])
+
+# range conversion - not so effective, but could be used elsewhere...
+def convert(old_value):
+	old_max = 1
+	old_min = 0
+	new_max = 255
+	new_min = 0
+	old_range = old_max - old_min
+	new_range = new_max - new_min
+	value = (((old_value - old_min) * new_range) / old_range) + new_min
+	return int(value)
 
 # Find minimum and maximum of image
 def find_min_max(array):
@@ -21,36 +46,56 @@ def normalise(array):
 # converting TIFF into arrays that we can work with
 def process_image(x, y, z):
 	# Open image, convert to numpy array
-	img = Image.open('data/voxels/vox390.MK.'+str(x)+'.'+str(y)+'.'+str(z)+'.tif')
+	img = Image.open('data/voxels/all_mk/vox390.MK.'+str(x)+'.'+str(y)+'.'+str(z)+'.tif')
 	numpy_array = numpy.array(img)
 	# image_array contains 260 arrays of values!
 	image_array = normalise(numpy_array)
 
 	return image_array
 
-
-image_1 = process_image(7, 22, 0)
-image_2 = process_image(7, 22, 1)
-image_3 = process_image(7, 22, 2)
-image_4 = process_image(7, 22, 3)
-image_5 = process_image(7, 22, 4)
-image_6 = process_image(7, 22, 5)
-
-images = [process_image(7, 22, x) for x in range(0,71)]
+# create an array of each image for this section
+images = [process_image(x_val, y_val, x) for x in range(0,z_val + 1)]
 
 
-def plot(images):
+def plot_cross(images, l):
+	# creating new grayscale image
 	new = Image.new('L', (260, 260))
 
+	# enumerate over images
 	for i, image in enumerate(images):
-		line = image[0]
+		# grab 'line' of image
+		line = image[l]
 		for x, value in enumerate(line):
-			new.putpixel((x, i), (value * 2550))
-	
-	new.show()
+			# place pixel in x position, at image index, with density value
+			new.putpixel((x, 259 - i), (value * (255 * 5)))
+	new.save("data/voxels/cross/mk-7-23/mk-7-23-{0:04d}.png".format(l))
 	return
 
-plot(images)
+def png_convert(images):
+	new = Image.new('RGBA', (260, 260))
 
+	for i, image in enumerate(images):
+		for x, line in enumerate(image):
+			for y, value in enumerate(line):
+				new.putpixel((x, y), ((convert(value)), (convert(value)), (convert(value)), (convert(value))))
+				w, h = new.size
+				new_image = new.crop((crop_x, crop_y, crop_x + size, crop_y + size))
+				resized = new_image.resize((50, 50), Image.ANTIALIAS)
+
+	
+		directory = "data/voxels/png/mk-"+str(x_val)+"-"+str(y_val)+"/crop/"+str(size) + str(crop_x) + str(crop_y)
+		if not os.path.exists(directory):
+			os.makedirs(directory)
+
+		filename = "mk"+str(i)+".png"
+		print directory, filename
+		resized.save(directory + "/" + filename)
+
+# creates 260 images
+# for i in range(0, 259):
+# 	# i = 'line' of image
+# 	png_convert(images, i)
+
+png_convert(images)
 
 
